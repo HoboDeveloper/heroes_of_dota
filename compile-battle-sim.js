7d0de2dbcd4af87cc17581713c73c7d3072d84cb
doctorgester <doctorgester@gmail.com>
2019-07-27 03:33:54 +0300

    Added compile-battle-sim.js

---
+++ compile-battle-sim.js
@@ -0,0 +1,15 @@
+const { compile, copy_sim, copy_unit_defs } = require("./compiler");
+
+(async () => {
+    console.time("Compile");
+
+    await compile("battle-sim");
+
+    copy_sim("client-web/dist/battle_sim.js");
+    copy_sim("server-remote/dist/battle_sim.js");
+    copy_sim("dist/content/panorama/scripts/custom_game/battle_sim.js");
+
+    copy_unit_defs();
+
+    console.timeEnd("Compile");
+})();
