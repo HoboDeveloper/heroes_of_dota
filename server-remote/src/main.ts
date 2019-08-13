8bffa3c3c185b5faf3443be3cadee67206742c25
doctorgester <doctorgester@gmail.com>
2019-08-14 01:50:19 +0300

    Removed host specifying

--- server-remote/src/main.ts
+++ server-remote/src/main.ts
@@ -4,19 +4,14 @@ console.log("Starting server");
 
 const args = process.argv.slice(2);
 
-let host = "127.0.0.1";
 let with_test_player = false;
 
 if (args.length > 0) {
-    args.shift();
-
     for (const arg of args) {
         if (arg == "with_test_player") {
             with_test_player = true;
-        } else if (arg.startsWith("host:")) {
-            host = arg.substring("host:".length);
         }
     }
 }
 
-start_server(host, with_test_player);
+start_server(with_test_player);
